a2178374a62c926895d5a497d89b2a68966e6bd5
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2014-02-19 21:20:58 +0100

    HTML crawler added

---
+++ crawler.py
@@ -0,0 +1,11 @@
+import urllib.request
+import json
+
+username = "arjen.swellengrebel"
+token = "CAACEdEose0cBAPcZArCo6JfiyfbM528GnIf1IwQOroc1Cfs4V3KHdMhMyQsoUUhRRbXLBIM3PdfWZBjZC7WQ3XBEoDAoPLr8SeqMx3gm01f1j9AxCFTGM8xKF6NGV9Jh5H7goxkyVOVjKgypHxGzsvoRNMjDP568pCTzecsQg6tp30EYGMd62gqsESnFagZBqR0ZB1gv36QZDZD"
+
+url = "https://graph.facebook.com/"+username+"/feed?access_token="+token
+
+response = urllib.request.urlopen(url)
+JSONdata = json.loads(response.read())
+
