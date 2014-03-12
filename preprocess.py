5f91f0eef9318b24be8c9acfe36102756566bbb9
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2014-03-12 12:07:07 +0100

    Uses the 10 most commonly occurring words
    This is probably still a bad feature set, but maybe it's clever enough?

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,7 @@
 import csv
 import string
 import re
+import collections
 
 def preprocess(csvFile):
     # read csv
@@ -10,6 +11,7 @@ def preprocess(csvFile):
         csvWriter = csv.writer(procCsv)
         header = csvReader.next()
         allWords = set()
+        allWordsCounting = collections.Counter()
         postDicts = []
         likes = []
         for row in csvReader:
@@ -23,20 +25,23 @@ def preprocess(csvFile):
                 t = token.lower()
                 postDict[t] = 1
                 tokens.add(t)
+                allWordsCounting[t] += 1
             postDicts.append(postDict)
             allWords |= tokens
         allWords = list(allWords)
+        allWords = [i for i, v in allWordsCounting.most_common(10)]
         #make a new header with allWords and features from header
         #TODO add other features besides just the words
-        newHeader = allWords[:200]
+        newHeader = allWords
         newHeader.append(header[3])
         csvWriter.writerow(newHeader)
         for pD in xrange(len(postDicts)):
             row = []
-            for w in allWords[:20]:
+            for w in allWords:
                 row.append(1.0*(w in postDicts[pD]))
             row.append(likes[pD])
             csvWriter.writerow(row)
+        print( "Preprocessing done." )
 
 if __name__ == '__main__':
     preprocess('real-data.csv')
