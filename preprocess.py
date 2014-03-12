86c89412d66c0dbcc6d1032be82de32adf287496
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2014-03-12 12:55:05 +0100

    Added time and length
    number of words in the post and hours since GMT midnight

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@ import csv
 import string
 import re
 import collections
+import datetime
 
 def preprocess(csvFile):
     # read csv
@@ -10,9 +11,10 @@ def preprocess(csvFile):
         procCsv = open('processed-data.csv', 'w')
         csvWriter = csv.writer(procCsv)
         header = csvReader.next()
-        allWords = set()
         allWordsCounting = collections.Counter()
         postDicts = []
+        postLengths = []
+        postTimes = []
         likes = []
         for row in csvReader:
             datarow = []
@@ -21,24 +23,32 @@ def preprocess(csvFile):
             post = re.sub('\s+', ' ', post).translate(None, string.punctuation)
             tokens = set()
             postDict = {}
-            for token in post.split():
+            tokenList = post.split()
+            for token in tokenList:
                 t = token.lower()
                 postDict[t] = 1
                 tokens.add(t)
                 allWordsCounting[t] += 1
             postDicts.append(postDict)
-            allWords |= tokens
-        allWords = list(allWords)
-        allWords = [i for i, v in allWordsCounting.most_common(10)]
+            postLengths.append(row[1])
+            date = datetime.datetime.strptime(row[2][11:19], "%H:%M:%S")
+            secs_since_midnight = (date - date.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
+            postTimes.append(secs_since_midnight/3600) # HAHAHAHA
+        allWords = [i for i, v in allWordsCounting.most_common(20)]
         #make a new header with allWords and features from header
         #TODO add other features besides just the words
-        newHeader = allWords
+        newHeader = allWords[:]
+        # Can't forget about [:]! Otherwise the header is a pointer to allwords!
+        newHeader.append("post length")
+        newHeader.append("post time")
         newHeader.append(header[3])
         csvWriter.writerow(newHeader)
         for pD in xrange(len(postDicts)):
             row = []
             for w in allWords:
                 row.append(1.0*(w in postDicts[pD]))
+            row.append(postLengths[pD])
+            row.append(postTimes[pD])
             row.append(likes[pD])
             csvWriter.writerow(row)
         print( "Preprocessing done." )
